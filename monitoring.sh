178951534f068c5d51c0e277e0d3abf131fc0c80
bazaluga <bazaluga@student.42.fr>
2024-01-29 18:24:38 +0100

    Add info

--- monitoring.sh
+++ monitoring.sh
@@ -4,7 +4,7 @@ wall $'\t#Architecture:' `uname -a` \
      $'\n\t#Memory Usage:' `free -m | awk 'NR==2 {printf "%d/%dMB (%.2f%%)", $3,$2,$3*100/$2}'` \
      $'\n\t#Disk Usage:' `df --block-size=G --total | awk '/total/ {printf "%d/%dGb (%d%%)", $3,$2,$5}'` \
      $'\n\t#CPU load:' `mpstat | awk '/all/ {printf "%.1f%%", 100-$13}'` \
-     $'\n\t#Last boot:' `last reboot -n1 --time-format iso | awk '/reboot/ {$0=$5;split($0,arr,"T");date=arr[1];split(arr[2],time,":");printf "%s %d:%d", date, time[1], time[2]}'` \
+     $'\n\t#Last boot:' `last reboot -n1 --time-format iso | awk '/reboot/ {$0=$5;split($0,arr,"T");date=arr[1];split(arr[2],time,":");printf "%s %d:%d", date, time[1], time[2]}'` #uptime -s does the same thing\
      $'\n\t#LVM use:' `awk '/\/dev\/mapper/ {} END{if (NR > 0) printf "yes"; else printf "no"}' /etc/fstab` \
      $'\n\t#Connections TCP:' `netstat -t | sed -n '/ESTABLISHED/p' | wc -l; printf " ESTABLISHED"` \
      $'\n\t#User log:' `users | awk '{print NF}'` \
